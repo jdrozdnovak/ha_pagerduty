2bcaaf332e69ed958106569314eadea1c113780a
Jan Drozd Novak <jan.drozdnovak@infor.com>
2023-12-20 14:45:30 +0100

    add debug

--- custom_components/pagerduty/config_flow.py
+++ custom_components/pagerduty/config_flow.py
@@ -1,9 +1,8 @@
 import logging
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant, callback
-from .const import CONF_API_TOKEN, CONF_TEAM_ID
-from .const import DOMAIN
+from homeassistant.core import callback
+from .const import CONF_API_TOKEN, CONF_TEAM_ID, DOMAIN
 from pdpyras import APISession, PDClientError
 
 _LOGGER = logging.getLogger(__name__)
@@ -11,25 +10,33 @@ _LOGGER = logging.getLogger(__name__)
 
 class PagerDutyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-    hass: HomeAssistant
 
     async def validate_input(self, hass, token):
         """Validate the user input allows us to connect."""
-        session = APISession(token)
-        try:
-            # Test API call; adjust endpoint as necessary
-            session.rget("abilities")
-            return True
-        except PDClientError:
-            return False
+
+        def validate():
+            session = APISession(token)
+            try:
+                # Perform a test API call
+                session.rget("abilities")
+                return True
+            except PDClientError as e:
+                _LOGGER.debug("Failed to validate PagerDuty token: %s", e)
+                return False
+
+        _LOGGER.debug("Validating PagerDuty API token")
+        return await hass.async_add_executor_job(validate)
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
+            _LOGGER.debug("Received user input: %s", user_input)
             valid = await self.validate_input(self.hass, user_input[CONF_API_TOKEN])
             if not valid:
                 errors["base"] = "invalid_auth"
+                _LOGGER.debug("Validation failed: invalid_auth")
             if not errors:
+                _LOGGER.debug("Validation successful, creating entry")
                 return self.async_create_entry(title="PagerDuty", data=user_input)
 
         data_schema = vol.Schema(
@@ -57,6 +64,7 @@ class PagerDutyOptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         """Manage the PagerDuty options."""
+        _LOGGER.debug("Initializing PagerDuty options flow")
         # Implement options flow if needed
         return self.async_show_form(
             step_id="init"
