5159c8958e319b84dab2b6c7893c32ef70e88188
Jan Drozd Novak <jan.novak@outlook.com>
2023-12-27 14:19:46 +0000

    add fixes

--- custom_components/pagerduty/const.py
+++ custom_components/pagerduty/const.py
@@ -1,6 +1,4 @@
-from datetime import timedelta
-
 """Constants for the PagerDuty integration."""
 DOMAIN = "pagerduty"
 CONF_API_KEY = "api_key"
-UPDATE_INTERVAL = timedelta(minutes=1)
+UPDATE_INTERVAL = 60
