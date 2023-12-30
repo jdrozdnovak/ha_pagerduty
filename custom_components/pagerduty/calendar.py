decbfb9e0b03633f0f0ed49e65d4e1d973eb07d7
Jan Drozd Novak <jan.novak@outlook.com>
2023-12-30 13:37:17 +0000

    working state

--- custom_components/pagerduty/calendar.py
+++ custom_components/pagerduty/calendar.py
@@ -1,5 +1,4 @@
 import logging
-from datetime import datetime, timedelta
 from homeassistant.components.calendar import CalendarEntity, CalendarEvent
 from homeassistant.util import dt as dt_util
 from .const import DOMAIN
@@ -36,16 +35,15 @@ class PagerDutyCalendar(CalendarEntity):
 
     async def async_get_events(self, hass, start_date, end_date):
         """Return events between start_date and end_date."""
-        self._events = []
         await self.coordinator.async_request_refresh()
-
-        schedules = await hass.async_add_executor_job(
+        
+        new_schedules = await hass.async_add_executor_job(
             self.coordinator.fetch_on_call_schedules,
             self.user_id,
             str(dt_util.DEFAULT_TIME_ZONE),
         )
 
-        for schedule_details in schedules:
+        for schedule_details in new_schedules:
             schedule_entries = schedule_details.get("final_schedule", {}).get(
                 "rendered_schedule_entries"
             ) or schedule_details.get("schedule_layers", [{}])[0].get(
@@ -56,30 +54,33 @@ class PagerDutyCalendar(CalendarEntity):
                 if entry.get("user", {}).get("id") == self.user_id:
                     start = self._parse_datetime(entry.get("start"))
                     end = self._parse_datetime(entry.get("end"))
-                    if (
-                        start
-                        and end
-                        and start <= end_date
-                        and end >= start_date
-                    ):
+                    if start and end and start <= end_date and end >= start_date:
                         unique_id_part = self._get_unique_id_part(
                             entry, schedule_details["id"]
                         )
+                        uid = f"{schedule_details['id']}-{unique_id_part}"
                         event = CalendarEvent(
                             summary=schedule_details["name"],
                             start=start,
                             end=end,
                             location=entry["user"]["summary"],
                             description=f"Schedule ID: {schedule_details['id']}",
-                            uid=f"{schedule_details['id']}-{unique_id_part}",
+                            uid=uid,
                         )
-                        self._events.append(event)
+                        self._update_or_add_event(event)
 
-        today_midnight = dt_util.now().replace(
-            hour=0, minute=0, second=0, microsecond=0
-        )
-        self._events = [e for e in self._events if e.end > today_midnight]
         return self._events
+
+    def _update_or_add_event(self, new_event):
+        """Update an existing event or add a new one based on UID."""
+        existing_uids = [event.uid for event in self._events]
+        if new_event.uid not in existing_uids:
+            self._events.append(new_event)
+        else:
+            for i, event in enumerate(self._events):
+                if event.uid == new_event.uid:
+                    self._events[i] = new_event
+                    break
 
     def _get_unique_id_part(self, entry, schedule_id):
         """Generate a unique part of ID for each entry."""
